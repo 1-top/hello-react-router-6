a79f2942973ee9bb954a12a85d3c4083981f1b07
1-top <best.no.1.top@gmail.com>
2022-07-27 16:38:55 +0900

    :sparkles: Search Params

--- src/components/UserDetails.tsx
+++ src/components/UserDetails.tsx
@@ -1,9 +1,17 @@
 import React from 'react';
+import { useParams } from 'react-router-dom';
+
+type UserDetailsParams = {
+  id: string;
+}
 
 const UserDetails = () => {
+  const params = useParams<UserDetailsParams>();
+  // const { id } = useParams<UserDetailsParams>();
   return (
     <div>
-      Details about Users
+      Details about Users {params.id}
+      {/*Details about Users {id}*/}
     </div>
   );
 };
--- src/components/Users.tsx
+++ src/components/Users.tsx
@@ -1,11 +1,21 @@
 import React from 'react';
+import { useSearchParams } from 'react-router-dom';
 
 const Users = () => {
+  const [searchParams, setSearchParams] = useSearchParams();
+  const isFilterActive = searchParams.get('filter') === 'active';
   return (
     <div>
       <h1>User 1</h1>
       <h1>User 2</h1>
       <h1>User 3</h1>
+      <div>
+        <button onClick={() => setSearchParams({ filter: 'active' })}>Active Users</button>
+        <button onClick={() => setSearchParams({})}>Reset Users</button>
+      </div>
+      {
+        isFilterActive ? <h2>Showing Active Users</h2> : <h2>Showing all users</h2>
+      }
     </div>
   );
 };
